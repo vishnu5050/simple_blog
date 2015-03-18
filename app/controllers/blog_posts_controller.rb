7d2b2ef93de7d3c2788d28969284ddf6e9f18aa1
Vishnu S <svishnu5050@gmail.com>
2015-03-18 07:57:11 +0000

    changed actions only for logged in users

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,5 +1,5 @@
 class BlogPostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :authenticate_user!, except: [:index]
   
   before_action :correct_user, only: [:destroy ,:edit , :update,]
 
