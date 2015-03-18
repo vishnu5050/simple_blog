4c4b010ea817901595ae9608ff4e1f1d90acc5c2
Vishnu S <svishnu5050@gmail.com>
2015-03-18 08:54:37 +0000

    truncate post content

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,6 @@
 class BlogPost < ActiveRecord::Base
   belongs_to :user 
-  validates :title,presence: true, length: {maximum: 70}
+  validates :title,presence: true, length: {maximum: 70},case_sensitive: false
   validates :post,presence: true, length: {maximum: 400,minimum: 25}
   
 def self.search(query)
